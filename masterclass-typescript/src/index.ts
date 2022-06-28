3a6ef92fb2aefaecd9accfa35be10e0596f89976
triangulodepascal <samuelchagasalves@gmail.com>
2022-06-28 11:10:05 -0300

    Learning how to run typescript

--- masterclass-typescript/src/index.ts
+++ masterclass-typescript/src/index.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('Hello World1'));
 
 app.listen(3333);
