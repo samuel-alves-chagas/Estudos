8e3835893a85867b76c8f77103905244da7dc268
triangulodepascal <samuelchagasalves@gmail.com>
2022-06-28 14:59:56 -0300

    Finishing study

--- masterclass-typescript/src/controller/UserController.ts
+++ masterclass-typescript/src/controller/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import EmailService from '../services/EmailService';
 
 const users = [
   { nome: 'Samuel', idade: 21 },
@@ -8,4 +9,12 @@ export default {
   async index(req: Request, res: Response) {
     return res.json(users);
   },
+
+  async create (req: Request, res:Response){
+    const emailService = new EmailService();
+
+    emailService.sendMail({to:{name: 'Samuel Alves', email: 'samuel@gmail.com'},message: {subject:'Bem-vindo ao sistema', body:'Seja bem-vindo'}})
+    
+    res.send()
+  }
 };
--- masterclass-typescript/src/routes.ts
+++ masterclass-typescript/src/routes.ts
@@ -4,4 +4,5 @@ import UserController from './controller/UserController';
 const routes = Router();
 
 routes.get('/users', UserController.index);
+routes.get('/users/create', UserController.create);
 export default routes;
