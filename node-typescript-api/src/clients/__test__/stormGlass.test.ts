ea94013b6bd8ee5c5d1929ad9293bd77c2f93c74
triangulodepascal <samuelchagasalves@gmail.com>
2022-07-05 14:19:33 -0300

    TDD for consuming external API

---
+++ node-typescript-api/src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,18 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_weather_3_hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = -33;
+    const lng = 151;
+
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
+  });
+});
