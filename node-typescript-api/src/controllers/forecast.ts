8f7971cc11b03c587f4482eaf45937220a9e6050
triangulodepascal <samuelchagasalves@gmail.com>
2022-07-05 11:23:34 -0300

    Starting the API with OvernightJS

--- node-typescript-api/src/controllers/forecast.ts
+++ node-typescript-api/src/controllers/forecast.ts
@@ -1,8 +1,12 @@
-describe('Beach forecast functional tests', () => {
-  it('should return a forecast with just a few times', async() => {
-    const { body, status } = await global.testRequest.get('/forecast');
-    expect(status).toBe(200);
-    expect(body).toEqual([{
+import { Controller, Get } from "@overnightjs/core";
+import { Request, Response } from "express";
+
+@Controller('forecast')
+export class ForecastController {
+
+  @Get('')
+  public getForecastForLoggedUser(_: Request, res: Response): void {
+    res.send([{
       "time": "2020-04-26T00:00:00+00:00",
       "forecast": [{
         "lat": -33.792726,
@@ -34,6 +38,6 @@ describe('Beach forecast functional tests', () => {
         "waveHeight": 0.46,
         "windDirection": 310.48
       }] }
-    ]);
-  });
-});
+    ])
+  }
+}
